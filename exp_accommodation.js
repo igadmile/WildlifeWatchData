0d90a039ca79a17b5fef4b79931384472b6178e0
Ivan Mile Katanović <ivan.katanovic@biom.hr>
2015-05-22 18:12:51 +0200

    added scenery, accomodation etc

---
+++ exp_accommodation.js
@@ -0,0 +1,25 @@
+var exp_accommodation = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "name": "Lika 131" }, "geometry": { "type": "Point", "coordinates": [ 15.29163, 44.64469, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Trnovac102" }, "geometry": { "type": "Point", "coordinates": [ 15.28207, 44.52179, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Lika 114" }, "geometry": { "type": "Point", "coordinates": [ 15.2988, 44.49057, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Kuterevo 119" }, "geometry": { "type": "Point", "coordinates": [ 15.14706, 44.82446, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Krbava 121" }, "geometry": { "type": "Point", "coordinates": [ 15.89516, 44.33588, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Kuterevo 112" }, "geometry": { "type": "Point", "coordinates": [ 15.13135, 44.83177, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Lika 115" }, "geometry": { "type": "Point", "coordinates": [ 15.26688, 44.5029, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Lika 124" }, "geometry": { "type": "Point", "coordinates": [ 15.16714, 44.5272, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Lika 118" }, "geometry": { "type": "Point", "coordinates": [ 15.24658, 44.5934, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Gacka 111" }, "geometry": { "type": "Point", "coordinates": [ 15.34738, 44.79498, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Krbava 128" }, "geometry": { "type": "Point", "coordinates": [ 15.84314, 44.29965, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Kuterevo 134" }, "geometry": { "type": "Point", "coordinates": [ 15.11869, 44.83863, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Pansion i restoran Albatros" }, "geometry": { "type": "Point", "coordinates": [ 15.38221, 44.66568, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Pansion i buffet Kristina" }, "geometry": { "type": "Point", "coordinates": [ 15.3819, 44.65617, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Kuća za odmor Krpan" }, "geometry": { "type": "Point", "coordinates": [ 15.37735, 44.64659, 0.0 ] } },
+{ "type": "Feature", "properties": { "name": "Sobe Tomaić" }, "geometry": { "type": "Point", "coordinates": [ 15.0547, 44.82076, 769.45 ] } },
+{ "type": "Feature", "properties": { "name": "Apartmani Manjan" }, "geometry": { "type": "Point", "coordinates": [ 15.052, 44.82128, 779.35 ] } },
+{ "type": "Feature", "properties": { "name": "Kuća za odmor" }, "geometry": { "type": "Point", "coordinates": [ 15.04806, 44.82161, 801.23 ] } }
+]
+}
---
+++ exp_scenery.js
@@ -0,0 +1,19 @@
+var exp_scenery = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 14.98548, 44.97832, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 15.08808, 44.82581, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 15.14351, 44.52879, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 14.90564, 44.69925, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 14.93693, 44.93963, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 14.94923, 44.93219, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 14.98449, 44.87147, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 15.65236, 44.29052, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 15.08633, 44.75555, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 15.05888, 44.61348, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 15.10453, 44.57847, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Vidikovac" }, "geometry": { "type": "Point", "coordinates": [ 15.10534, 44.55072, 0.0 ] } }
+]
+}
